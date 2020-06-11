543258cd8d5c3f6b1307b7461ae390e3df44b25d
Kaitlyn Schlegel <kaitlynjschlegel@gmail.com>
2020-06-11 16:45:49 -0500

    added random array length function and set up generateCharacters function

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -5,31 +5,46 @@ public class app {
     }
 
     private void run() {
-        Child childSurvivor = new Child();
-        Child secondChild = new Child();
-        Teacher teacherSurvivor = new Teacher();
-        Teacher secondTeacher = new Teacher();
-        Teacher thirdTeacher = new Teacher();
-        Soldier soldierSurvivor = new Soldier();
-        Tank tankZombie = new Tank();
-        CommonInfect commonInfectZombie = new CommonInfect();
-
-        System.out.println("Child character stats at creation");
-        System.out.println(childSurvivor);
-        childSurvivor.setHealth(15);
+    	// generate the two character arrays
+    	Character[] zombies = generateCharacters("zombie");
+    	Character[] survivors = generateCharacters("survivor");
+    	// test to make sure the characters are in the array
+    	System.out.println(" ");
+        for (Character c : zombies) {
+        	System.out.println(c);
+        }
         System.out.println(" ");
-        
-        System.out.println("Child character stats after health dropped by 5");
-        System.out.println(childSurvivor);
-        System.out.println(" ");
-        
-        // show that the names are incrementing
-        System.out.println(secondChild);
-        System.out.println(secondTeacher);
-        System.out.println(thirdTeacher);
-        
-        System.out.println(" ");
-        System.out.println("Tank attacking");
-        tankZombie.attack();
+        for (Character c : survivors) {
+        	System.out.println(c);
+        }
+    }
+    
+    // generate an array of characters between 5 and 20 elements in length
+    private Character[] generateCharacters(String charType) {
+    	Character[] characters = new Character[getRandom(5, 20)];
+    	System.out.println(charType + " array length: " + characters.length);
+    	if (charType.contentEquals("zombie")) {
+    		// generate the random zombies that will fill the array
+    		characters[0] = new Tank();
+    		characters[1] = new Tank();
+    		characters[2] = new CommonInfect();
+    	} else if (charType.contentEquals("survivor")) {
+    		// generate the random survivors that will fill the array
+    		characters[0] = new Child();
+    		characters[1] = new Child();
+    		characters[2] = new Teacher();
+    		characters[3] = new Soldier();
+    		characters[4] = new Soldier();
+    	} else {
+    		// the type passed to the function was neither zombies nor survivors
+    		System.out.println(charType + " is an invalid character type.");
+    	}
+    	return characters;
+    }
+    
+    // I figured it would be easier and cleaner to have a function to get the random numbers
+    // than it would have been to use Math.random() everywhere.
+    private int getRandom(int min, int max) {
+    	return (int)(Math.random() * (max - min + 1) + min); 
     }
 }
