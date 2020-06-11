fdb9c39056733196966bdb83394fee4e28b70cde
Kaitlyn Schlegel <kaitlynjschlegel@gmail.com>
2020-06-11 18:18:36 -0500

    added the random characters into each of the two arrays

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -8,33 +8,61 @@ public class app {
     	// generate the two character arrays
     	Character[] zombies = generateCharacters("zombie");
     	Character[] survivors = generateCharacters("survivor");
-    	// test to make sure the characters are in the array
-    	System.out.println(" ");
-        for (Character c : zombies) {
-        	System.out.println(c);
-        }
-        System.out.println(" ");
-        for (Character c : survivors) {
-        	System.out.println(c);
-        }
+    	
+    	System.out.println("We have " + survivors.length + " survivors trying to make it to safety.");
+    	System.out.println("But there are " + zombies.length + " zombies waiting for them.");
     }
     
     // generate an array of characters between 5 and 20 elements in length
     private Character[] generateCharacters(String charType) {
     	Character[] characters = new Character[getRandom(5, 20)];
-    	System.out.println(charType + " array length: " + characters.length);
     	if (charType.contentEquals("zombie")) {
+    		// initialize counter variables
+    		int tankCount = 0;
+    		int commonCount = 0;
     		// generate the random zombies that will fill the array
-    		characters[0] = new Tank();
-    		characters[1] = new Tank();
-    		characters[2] = new CommonInfect();
+    		for (int i=0; i<characters.length; i++) {
+    			// get a random number that will dictate which character type is put into the array
+    			int randomZombie = getRandom(0, 1);
+    			switch (randomZombie) {
+    			case 0:
+    				characters[i] = new CommonInfect();
+    				commonCount++;
+    				break;
+    			case 1:
+    				characters[i] = new Tank();
+    				tankCount++;
+    				break;
+    			default:
+    				System.out.println("The random character was neither 0 nor 1");
+    			} // end switch statement for getting the zombie types
+    		} // end for loop for populating the zombie array
     	} else if (charType.contentEquals("survivor")) {
+    		// initialize counter variables
+    		int childCount = 0;
+    		int teacherCount = 0;
+    		int soldierCount = 0;
     		// generate the random survivors that will fill the array
-    		characters[0] = new Child();
-    		characters[1] = new Child();
-    		characters[2] = new Teacher();
-    		characters[3] = new Soldier();
-    		characters[4] = new Soldier();
+    		for(int i=0; i<characters.length; i++) {
+    			// get a random number that will dictate which character type is put into the array
+    			int randomSurvivor = getRandom(0,2);
+    			switch(randomSurvivor) {
+    			case 0:
+    				characters[i] = new Child();
+    				childCount++;
+    				break;
+    			case 1:
+    				characters[i] = new Teacher();
+    				teacherCount++;
+    				break;
+    			case 2:
+    				characters[i] = new Soldier();
+    				soldierCount++;
+    				break;
+    			default:
+    				System.out.println("The random character was neither 0, 1, nor 2");
+    			} // end switch statement for getting the survivor types
+    		} // end for loop for populating the survivor array
     	} else {
     		// the type passed to the function was neither zombies nor survivors
     		System.out.println(charType + " is an invalid character type.");
