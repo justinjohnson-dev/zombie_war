f68a9181b6c09d51de275b2d94b61b0129ea788b
Justin Johnson <jjustin634@gmail.com>
2020-06-12 17:50:40 -0500

    adding some loops to the final conditional to return the response needed for release one

--- src/main/java/app.java
+++ src/main/java/app.java
@@ -114,9 +114,6 @@ public class app {
             }
 
             if (zombie.getHealth() > 0 || survivor.getHealth() > 0) {
-                System.out.println(numberOfHealthyZombies);
-                System.out.println(numberOfHealthySurvivors);
-
                 for (int i = 0; i < zombie_list.size(); i++) {
                     Character zombieAttacked = zombie_list.get(i);
                     // Check is zombieAttacked is still alive
@@ -165,9 +162,23 @@ public class app {
         }
 
         if (numberOfHealthySurvivors > numberOfHealthyZombies) {
-            return "Survivors won!!";
+            int survivorCounter = 0;
+            for (int i = 0; i < survivor_list.size(); i++) {
+                Character survivor = survivor_list.get(i);
+                if (survivor.getHealth() > 0) {
+                    survivorCounter++;
+                }
+            }
+            return "It seems " + survivorCounter +  " have made it to safety.";
         } else {
-            return "Zombies won!!";
+            int zombieCounter = 0;
+            for (int i = 0; i < zombie_list.size(); i++) {
+                Character zombie = zombie_list.get(i);
+                if (zombie.getHealth() > 0) {
+                    zombieCounter++;
+                }
+            }
+            return "It seems " + zombieCounter +  " have made it to safety.";
         }
     }
 }
